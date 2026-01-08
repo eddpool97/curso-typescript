cf5389c3ec429d369493a0ac43c78d3041374598
eddpool97 <78087019+eddpool97@users.noreply.github.com>
2026-01-08 19:34:44 +0000

    Inicio del curso de TypeScript

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Hello World in TypeScript");
+console.log("Hello Again");
+console.log('Hello One More Time');
---
+++ app.ts
@@ -0,0 +1,7 @@
+
+
+console.log("Hello World in TypeScript");
+
+console.log("Hello Again");
+
+console.log('Hello One More Time');
